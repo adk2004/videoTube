4b590a221b818b411c04213037443c6126a9762e
adk2004 <dhingraaksh@gmail.com>
2024-12-09 13:11:55 +0530

    Add Util files

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import e from "express";
+import express, { urlencoded } from "express";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "20kb" }));
+app.use(urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -2,13 +2,25 @@
 
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-import express from "express";
-const app = express();
+import { app } from "./app.js";
+
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.on("error", (err) => {
+      console.log("Error : can not talk to DB", err);
+      throw err;
+    });
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+})
+.catch ((err)=> {
+    console.error("Mongo DB connection error : ", err)
+})
 
 
 
