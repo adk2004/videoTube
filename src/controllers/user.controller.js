409c667835f62404c3466d15de9af5017c66cf56
adk2004 <dhingraaksh@gmail.com>
2024-12-11 17:15:10 +0530

    add user register controller and fixed some errors

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,60 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+
+const registerUser = asyncHandler(async (req, res) => {
+  // get user details from rq body
+  // validation - not empty
+  //check duplicay for email, username
+  // check for images avatar(compulsory)
+  //upload imag eto cloudinary, avatar
+  //create user object -- create enrty in DB
+  // insert into DB
+  // remove password and refresh token field from response
+  // return response
+
+  const { fullName, email, username, password } = req.body;
+  console.log("Full Name : ", fullName);
+
+  if (
+    [fullName, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+  const existedUser = User.findOne({
+    $or: [{ email }, { username }],
+  });
+  if (existedUser) {
+    throw new ApiError(409, "This username or email id is already in use");
+  }
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar is required");
+  }
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  const user = await User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    username: username.toLowerCase(),
+    password,
+  });
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+  if (!createdUser) {
+    throw new ApiError(500, "Something went Wrong while creating the user");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User Registered Successfully"));
+});
+
+export { registerUser };
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -12,11 +12,12 @@ const uploadOnCloudinary = async (localFilePath) => {
     if (!localFilePath) return null;
     //upload the file on cloudinary
     const response = await cloudinary.uploader.upload(localFilePath, {
-      resource_type: "auto",
+      resource_type: "auto",// detects wether the file is image audi pdf etc etc..
     });
     //file has been uploaded successfully
     console.log("File has been uloaded successfully", response.url);
     fs.unlinkSync(localFilePath); // remove the locally saved file after successful upload
+    console.log(response);
     return response;
   } catch (error) {
     fs.unlinkSync(localFilePath) // remove the locally saved file in case of failure
